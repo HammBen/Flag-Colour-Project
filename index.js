482713e2a5cfb1cf719c6aaa4139f0f6d639ceed
Benjamin Hammond <benjaminhammond268@gmail.com>
2023-04-04 14:54:37 +0000

    project left with color code

---
+++ index.js
@@ -0,0 +1,27 @@
+/* Get Target element */
+let searchBtn = document.getElementById("search-btn");
+let countryInp = document.getElementById("country-name-input");
+
+/* Add Event Listener to search Button */
+searchBtn.addEventListener("click", () => {
+  let countryName = countryInp.value;
+  let URL = `https://restcountries.com/v3.1/name/${countryName}`;
+
+//   console.log(URL.indexOf);
+
+  fetch(URL)
+    .then((response) => response.json())
+    .then((data) => {
+      result.innerHTML = `
+        <img src="${data[0].flags.svg}" class="flag-img">
+        <h2>${data[0].name.common}</h2>
+      `;
+    })
+    .catch(() => {
+      if (countryName.length == 0) {
+        result.innerHTML = `<h3>The input field cannot be empty</h3>`;
+      } else {
+        result.innerHTML = `<h3>Please enter a valid country name.</h3>`;
+      }
+    });
+});
